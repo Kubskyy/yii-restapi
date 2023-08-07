807de1be633b1af0ea037c12d7512c5d32602a27
Jakub Rutkowski <kubaa.rutkowski123@gmail.com>
2023-08-07 14:43:16 +0200

    Initial commit

---
+++ frontend/controllers/PostController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace frontend\controllers;
+
+use frontend\resource\Post;
+use yii\rest\ActiveController;
+
+class PostController extends ActiveController
+{
+    public $modelClass = Post::class;
+
+}
